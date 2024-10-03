99b9fff88a547f90869cff428c3052497fd61205
Tkhangdi <tkhangdi2@gmail.com>
2024-10-03 16:29:24 +0700

    Them Trang Ve Chung Toi

--- NhaXe_QuocVuong/Controllers/HomeController.cs
+++ NhaXe_QuocVuong/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace NhaXe_QuocVuong.Controllers
         {
             return View();
         }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
