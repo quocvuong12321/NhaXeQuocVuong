f6c46027a3bd923fc75b4c9f6cb0e98f220e1a8f
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-10-04 07:48:00 +0700

    Them area admin

---
+++ NhaXe_QuocVuong/Areas/Area_Admin/Area_AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NhaXe_QuocVuong.Areas.Area_Admin
+{
+    public class Area_AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Area_Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Area_Admin_default",
+                "Area_Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
