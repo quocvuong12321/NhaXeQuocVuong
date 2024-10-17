a534a7d7412a682a937b5337ad6a31d2916add1c
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-10-17 14:35:38 +0700

    add linq to sql server

---
+++ NhaXe_QuocVuong/Controllers/LichTrinhController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NhaXe_QuocVuong.Models;
+namespace NhaXe_QuocVuong.Controllers
+{
+    public class LichTrinhController : Controller
+    {
+        // GET: LichTrinh
+        NhaXeDataContext db = new NhaXeDataContext();
+        public ActionResult Index()
+        {
+            
+            return View(db.LichTrinhs.ToList());
+        }
+    }
+}
