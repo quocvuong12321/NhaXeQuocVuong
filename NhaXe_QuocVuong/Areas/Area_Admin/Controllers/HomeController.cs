2d80b4a881a2c4255ac1a8a37eeb01c6261905c0
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-10-20 12:45:34 +0700

    Them layout admin

---
+++ NhaXe_QuocVuong/Areas/Area_Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NhaXe_QuocVuong.Areas.Area_Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Area_Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
